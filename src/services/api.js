1552141c87c67a87f509afa2606bdc09ba0c3343
Guilherme0893 <guilhermeduarte008@gmail.com>
2021-11-18 17:10:15 -0300

    Requisito 01 -- testes OK

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,14 @@
 export async function getCategories() {
-  // Implemente aqui
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => data);
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  // usei o template aqui para pegar os parametros dados pela Trybe e inserir na url
+  const url = ` https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => data);
 }
