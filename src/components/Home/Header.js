f0058bd42e19dc9bc351f64dc8b61c9e352e30ae
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-06-06 00:17:53 -0300

    style: Add responsive style components and adds react bootstrap tags

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -2,7 +2,7 @@ import React, { useEffect, useContext } from 'react';
 import { BsCartFill, BsCart } from 'react-icons/bs';
 import PropTypes from 'prop-types';
 import { Link, useHistory } from 'react-router-dom';
-import { Form, FormControl, Button, Navbar } from 'react-bootstrap';
+import { Form, FormControl, Button, Nav } from 'react-bootstrap';
 import GlobalContext from '../../Context/GlobalContext';
 import Dropdown from './DropdownMenu';
 
@@ -28,68 +28,49 @@ function Header(props) {
   });
 
   return (
-    <header
+    <Nav
       data-testid="main-header"
+      className="d-flex justify-content-evenly p-3"
       style={ {
         backgroundColor: 'rgb(54, 69, 79)',
         border: '1px solid black',
       } }
     >
-      <div>
-        <Navbar
-          className="d-flex justify-content-center"
+      <Form
+        className="d-flex"
+      >
+        <FormControl
           style={ {
-            position: '',
-            height: 'auto',
+            marginLeft: '50px',
           } }
-        >
-          <Link to="/" className="text-white" style={ { textDecoration: 'none' } }>
-            <h1>
-              { text }
-            </h1>
-          </Link>
-          <Form
-            className="d-flex"
-          >
-            <FormControl
-              style={ {
-                marginLeft: '50px',
-              } }
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
+          type="search"
+          placeholder="Search"
+          className="me-2"
+          aria-label="Search"
+        />
+        <Button disabled>Search</Button>
+      </Form>
+      <Link to="/" className="text-white" style={ { textDecoration: 'none' } }>
+        <h1>
+          { text }
+        </h1>
+      </Link>
+      <Dropdown />
+      <Link href="null" style={ { maxWidth: '30px' } }>
+        { cartEmpty ? (
+          <BsCartFill
+            onClick={ () => onButtonClick() }
+            size="1x"
+          />
+        )
+          : (
+            <BsCart
+              onClick={ () => onButtonClick() }
+              size="1x"
             />
-            <Button>Search</Button>
-          </Form>
-        </Navbar>
-        <Navbar
-          className="d-flex justify-content-center"
-          style={ { margin: 'auto' } }
-        >
-          <Dropdown />
-          <div
-            className=""
-            style={ { maxWidth: '30px' } }
-          >
-            <Link href="null">
-              { cartEmpty ? (
-                <BsCartFill
-                  onClick={ () => onButtonClick() }
-                  size="1x"
-                />
-              )
-                : (
-                  <BsCart
-                    onClick={ () => onButtonClick() }
-                    size="1x"
-                  />
-                )}
-            </Link>
-          </div>
-        </Navbar>
-      </div>
-    </header>
+          )}
+      </Link>
+    </Nav>
   );
 }
 
--- src/components/Home/ProductCard.js
+++ src/components/Home/ProductCard.js
@@ -10,32 +10,30 @@ function ProductCard(props) {
   return (
     <Card
       id="productCard"
-      className="m-2 mt-2 overflow-hidden"
+      className="m-2 mt-2 overflow-hidden shadow-lg"
       style={ {
         backgroundColor: 'RGB(233, 236, 239)',
       } }
     >
       <Card className="d-flex flex-column align-items-center">
-        <div className="pb-2">
-          <Link to={ `/products/${id}` }>
-            <img
-              className="img-thumbnail mx-auto d-block"
-              style={ {
-                width: '15rem',
-                height: '15rem',
-                borderBottom: '2px solid gray',
-              } }
-              src={ thumbnail }
-              alt={ title }
-            />
-          </Link>
-          <h5 className="d-flex pt-2 justify-content-center">
-            R$
-            {
-              price
-            }
-          </h5>
-        </div>
+        <Link to={ `/products/${id}` }>
+          <Card.Img
+            className="img-thumbnail mx-auto d-block"
+            style={ {
+              width: '15rem',
+              height: '15rem',
+              borderBottom: '2px solid gray',
+            } }
+            src={ thumbnail }
+            alt={ title }
+          />
+        </Link>
+        <Card.Text className="fw-bold d-flex pt-2 pb-2 justify-content-center">
+          R$
+          {
+            price
+          }
+        </Card.Text>
       </Card>
     </Card>
   );
@@ -46,7 +44,6 @@ ProductCard.propTypes = {
   price: PropTypes.number.isRequired,
   id: PropTypes.string.isRequired,
   thumbnail: PropTypes.string.isRequired,
-  // available_quantity: PropTypes.number.isRequired,
 };
 
 export default ProductCard;
