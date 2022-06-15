1129258c354933a74cb91aab3b4fadc64d2be259
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-06-15 18:27:39 -0300

    chore: Adds loading spinning into main page

--- src/components/Home/MainProducts.js
+++ src/components/Home/MainProducts.js
@@ -1,4 +1,7 @@
+/* eslint-disable react/jsx-indent-props */
+/* eslint-disable react/jsx-closing-tag-location */
 import React, { useEffect, useContext } from 'react';
+import { Spinner } from 'react-bootstrap';
 import GlobalContext from '../../Context/GlobalContext';
 import ProductCard from './ProductCard';
 
@@ -18,20 +21,28 @@ function MainProducts() {
 
   return (
     <div className="w-75 mx-auto">
-      <main
-        className="d-flex flex-wrap justify-content-between"
-      >
-        {mainProducts.map((products) => (
-          <ProductCard
-            key={ products.id }
-            thumbnail={ products.thumbnail }
-            title={ products.title }
-            price={ products.price }
-            available_quantity={ products.available_quantity }
-            id={ products.id }
-          />
-        ))}
-      </main>
+      {
+        mainProducts.length === 0
+          ? <div className="d-flex justify-content-center">
+            <Spinner animation="border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          </div>
+          : <main
+            className="d-flex flex-wrap justify-content-between"
+          >
+            {mainProducts.map((products) => (
+              <ProductCard
+                key={ products.id }
+                thumbnail={ products.thumbnail }
+                title={ products.title }
+                price={ products.price }
+                available_quantity={ products.available_quantity }
+                id={ products.id }
+              />
+            ))}
+          </main>
+      }
     </div>
   );
 }
