62092a6bc590ef0a46ed56b1f8556da04835dc0c
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-06-15 18:42:56 -0300

    refactor: Changes the structure of the ternary condition of the spinner

--- src/components/Home/MainProducts.js
+++ src/components/Home/MainProducts.js
@@ -1,5 +1,3 @@
-/* eslint-disable react/jsx-indent-props */
-/* eslint-disable react/jsx-closing-tag-location */
 import React, { useEffect, useContext } from 'react';
 import { Spinner } from 'react-bootstrap';
 import GlobalContext from '../../Context/GlobalContext';
@@ -22,26 +20,29 @@ function MainProducts() {
   return (
     <div className="w-75 mx-auto">
       {
-        mainProducts.length === 0
-          ? <div className="d-flex justify-content-center">
+        mainProducts.length === 0 ? (
+          <div className="d-flex justify-content-center">
             <Spinner animation="border" role="status">
               <span className="visually-hidden">Loading...</span>
             </Spinner>
           </div>
-          : <main
-            className="d-flex flex-wrap justify-content-between"
-          >
-            {mainProducts.map((products) => (
-              <ProductCard
-                key={ products.id }
-                thumbnail={ products.thumbnail }
-                title={ products.title }
-                price={ products.price }
-                available_quantity={ products.available_quantity }
-                id={ products.id }
-              />
-            ))}
-          </main>
+        )
+          : (
+            <main
+              className="d-flex flex-wrap justify-content-between"
+            >
+              {mainProducts.map((products) => (
+                <ProductCard
+                  key={ products.id }
+                  thumbnail={ products.thumbnail }
+                  title={ products.title }
+                  price={ products.price }
+                  available_quantity={ products.available_quantity }
+                  id={ products.id }
+                />
+              ))}
+            </main>
+          )
       }
     </div>
   );
